159584c23d7666db920ea0bf089fec77bf7c4042
Phinehastephen <stephenokonkwo18@gmail.com>
2026-01-01 08:16:20 +1300

    updates

--- config.py
+++ config.py
@@ -1,15 +1,13 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "defaultsecretkey123")
+    MYSQL_HOST = os.environ.get("MYSQL_ADDON_HOST", "localhost")
+    MYSQL_USER = os.environ.get("MYSQL_ADDON_USER", "root")
+    MYSQL_PASSWORD = os.environ.get("MYSQL_ADDON_PASSWORD", "")
+    MYSQL_DB = os.environ.get("MYSQL_ADDON_DB", "users")
+    MYSQL_PORT = int(os.environ.get("MYSQL_ADDON_PORT", 3306))
 
-    MYSQL_ADDON_DB="bwnpzqcu6wxwiwysgjfj"
-    MYSQL_ADDON_HOST="bwnpzqcu6wxwiwysgjfj-mysql.services.clever-cloud.com"
-    MYSQL_ADDON_PASSWORD="KokQmeOczwRowRaihSh5"
-    MYSQL_ADDON_PORT="3306"
-    MYSQL_ADDON_URI="mysql://uefgl8bfwgzw275l:KokQmeOczwRowRaihSh5@bwnpzqcu6wxwiwysgjfj-mysql.services.clever-cloud.com:3306/bwnpzqcu6wxwiwysgjfj"
-    MYSQL_ADDON_USER="uefgl8bfwgzw275l"
-    MYSQL_ADDON_VERSION="8.0"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "development-secret")
 
     UPLOAD_FOLDER = "static/uploads"
     ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
